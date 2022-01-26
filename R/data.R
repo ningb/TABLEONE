a162e5c67c14aa3d3d049aa5edb3e08da9cdf94c
ningb <ningb@bu.edu>
2022-01-25 19:34:43 -0500

    Fix DESCRIPTION

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Toy example for descriptive data
+#'
+#'
+#' @format A data frame with 235 rows and 6 variables:
+#' #' \describe{
+#'   \item{RIN}{RIN}
+#'   \item{Gender}{Gender}
+#'   \item{Age}{Age}
+#'   \item{Packyear}{Packyear}
+#'   \item{Smoking}{Smoking Status}
+#'   \item{Cancer}{Cancer Status}
+#' }
+"index"
